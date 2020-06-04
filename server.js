d494de1025cdfd7657aade3fc4892c7278cef674
Philip <philipwilcox@users.noreply.github.com>
2020-06-04 13:34:02 -0700

    Change to use node to match spotify guide for initial oauth ease (#1)

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8888;
+
+// TODO: pull client id and secret from ENV vars https://nodejs.dev/learn/how-to-read-environment-variables-from-nodejs
+// TODO: learn about async https://nodejs.dev/learn/modern-asynchronous-javascript-with-async-and-await
+// TODO: http server setup https://nodejs.dev/learn/build-an-http-server
+// TODO: making http request https://nodejs.dev/learn/making-http-requests-with-nodejs
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    let responseBody = (req.url == '/callback') ? callbackInfoToString(req) : `Hello World from ${req.url}`
+    res.end(responseBody);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function callbackInfoToString(req) {
+    return `Hi this is the callback ${req.url}`;
+}
